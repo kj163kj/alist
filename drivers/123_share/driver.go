c3c088178b17e8e8fdb95c7a6a98294f4b824fff
Tech_Sky <121146755+kj163kj@users.noreply.github.com>
2025-06-08 18:32:11 +0800

    Update driver.go

--- drivers/123_share/driver.go
+++ drivers/123_share/driver.go
@@ -1,83 +1,28 @@
+/*
+ * drivers/123_share/driver.go
+ */
 package _123Share
 
 import (
 	"context"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
-	"sync"
-	"time"
+	"strings"
 
-	"golang.org/x/time/rate"
-
-	_123 "github.com/alist-org/alist/v3/drivers/123"
-	"github.com/alist-org/alist/v3/drivers/base"
-	"github.com/alist-org/alist/v3/internal/driver"
-	"github.com/alist-org/alist/v3/internal/errs"
-	"github.com/alist-org/alist/v3/internal/model"
-	"github.com/alist-org/alist/v3/pkg/utils"
 	"github.com/go-resty/resty/v2"
-	log "github.com/sirupsen/logrus"
+	"github.com/json-iterator/go"
+
+	"github.com/alist-org/alist/v3/internal/driver/base"
+	"github.com/alist-org/alist/v3/pkg/model"
+	"github.com/alist-org/alist/v3/pkg/utils"
+	"github.com/alist-org/alist/v3/pkg/log"
 )
 
-type Pan123Share struct {
-	model.Storage
-	Addition
-	apiRateLimit sync.Map
-	ref          *_123.Pan123
-}
-
-func (d *Pan123Share) Config() driver.Config {
-	return config
-}
-
-func (d *Pan123Share) GetAddition() driver.Additional {
-	return &d.Addition
-}
-
-func (d *Pan123Share) Init(ctx context.Context) error {
-	// TODO login / refresh token
-	//op.MustSaveDriverStorage(d)
-	return nil
-}
-
-func (d *Pan123Share) InitReference(storage driver.Driver) error {
-	refStorage, ok := storage.(*_123.Pan123)
-	if ok {
-		d.ref = refStorage
-		return nil
-	}
-	return fmt.Errorf("ref: storage is not 123Pan")
-}
-
-func (d *Pan123Share) Drop(ctx context.Context) error {
-	d.ref = nil
-	return nil
-}
-
-func (d *Pan123Share) List(ctx context.Context, dir model.Obj, args model.ListArgs) ([]model.Obj, error) {
-	// TODO return the files list, required
-	files, err := d.getFiles(ctx, dir.GetID())
-	if err != nil {
-		return nil, err
-	}
-	return utils.SliceConvert(files, func(src File) (model.Obj, error) {
-		return src, nil
-	})
-}
-
 func (d *Pan123Share) Link(ctx context.Context, file model.Obj, args model.LinkArgs) (*model.Link, error) {
-	// TODO return link of file, required
 	if f, ok := file.(File); ok {
-		//var resp DownResp
-		var headers map[string]string
-		if !utils.IsLocalIPAddr(args.IP) {
-			headers = map[string]string{
-				//"X-Real-IP":       "1.1.1.1",
-				"X-Forwarded-For": args.IP,
-			}
-		}
 		data := base.Json{
 			"shareKey":  d.ShareKey,
 			"SharePwd":  d.SharePwd,
@@ -86,8 +31,19 @@ func (d *Pan123Share) Link(ctx context.Context, file model.Obj, args model.LinkA
 			"s3keyFlag": f.S3KeyFlag,
 			"size":      f.Size,
 		}
+		var headers map[string]string
+		if !utils.IsLocalIPAddr(args.IP) {
+			headers = map[string]string{
+				"X-Forwarded-For": args.IP,
+			}
+		}
 		resp, err := d.request(DownloadInfo, http.MethodPost, func(req *resty.Request) {
-			req.SetBody(data).SetHeaders(headers)
+			req.SetBody(data).
+				SetHeaders(headers).
+				SetHeader("User-Agent", "123pan/v2.4.0(Android_10.0;Xiaomi)").
+				SetHeader("platform", "android").
+				SetHeader("app-version", "61").
+				SetHeader("x-app-version", "2.4.0")
 		}, nil)
 		if err != nil {
 			return nil, err
@@ -104,18 +60,23 @@ func (d *Pan123Share) Link(ctx context.Context, file model.Obj, args model.LinkA
 			if err != nil {
 				return nil, err
 			}
+			q := u.Query()
+			q.Set("auto_redirect", "0")
+			u.RawQuery = q.Encode()
 		}
 		u_ := u.String()
 		log.Debug("download url: ", u_)
-		res, err := base.NoRedirectClient.R().SetHeader("Referer", "https://www.123pan.com/").Get(u_)
+		res, err := base.NoRedirectClient.R().
+			SetHeader("Referer", "https://www.123pan.com/").
+			SetHeader("User-Agent", "123pan/v2.4.0(Android_10.0;Xiaomi)").
+			SetHeader("platform", "android").
+			SetHeader("app-version", "61").
+			SetHeader("x-app-version", "2.4.0").
+			Get(u_)
 		if err != nil {
 			return nil, err
 		}
-		log.Debug(res.String())
-		link := model.Link{
-			URL: u_,
-		}
-		log.Debugln("res code: ", res.StatusCode())
+		link := model.Link{URL: u_}
 		if res.StatusCode() == 302 {
 			link.URL = res.Header().Get("location")
 		} else if res.StatusCode() < 300 {
@@ -128,47 +89,3 @@ func (d *Pan123Share) Link(ctx context.Context, file model.Obj, args model.LinkA
 	}
 	return nil, fmt.Errorf("can't convert obj")
 }
-
-func (d *Pan123Share) MakeDir(ctx context.Context, parentDir model.Obj, dirName string) error {
-	// TODO create folder, optional
-	return errs.NotSupport
-}
-
-func (d *Pan123Share) Move(ctx context.Context, srcObj, dstDir model.Obj) error {
-	// TODO move obj, optional
-	return errs.NotSupport
-}
-
-func (d *Pan123Share) Rename(ctx context.Context, srcObj model.Obj, newName string) error {
-	// TODO rename obj, optional
-	return errs.NotSupport
-}
-
-func (d *Pan123Share) Copy(ctx context.Context, srcObj, dstDir model.Obj) error {
-	// TODO copy obj, optional
-	return errs.NotSupport
-}
-
-func (d *Pan123Share) Remove(ctx context.Context, obj model.Obj) error {
-	// TODO remove obj, optional
-	return errs.NotSupport
-}
-
-func (d *Pan123Share) Put(ctx context.Context, dstDir model.Obj, stream model.FileStreamer, up driver.UpdateProgress) error {
-	// TODO upload file, optional
-	return errs.NotSupport
-}
-
-//func (d *Pan123Share) Other(ctx context.Context, args model.OtherArgs) (interface{}, error) {
-//	return nil, errs.NotSupport
-//}
-
-func (d *Pan123Share) APIRateLimit(ctx context.Context, api string) error {
-	value, _ := d.apiRateLimit.LoadOrStore(api,
-		rate.NewLimiter(rate.Every(700*time.Millisecond), 1))
-	limiter := value.(*rate.Limiter)
-
-	return limiter.Wait(ctx)
-}
-
-var _ driver.Driver = (*Pan123Share)(nil)
